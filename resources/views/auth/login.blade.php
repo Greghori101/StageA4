4f2e43cbc544a3442c4fe427311301864747e977
Greghori101 <e.souala@esi-sba.dz>
2025-02-16 20:54:46 +0100

    add register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,11 +15,11 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adresse Email') }}</label>
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                       name="email" value="{{ old('email') }}" required autofocus>
+                                    name="email" value="{{ old('email') }}" required autofocus>
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -28,11 +28,11 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                                       name="password" required>
+                                    name="password" required>
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -41,7 +41,7 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                           {{ old('remember') ? 'checked' : '' }}>
+                                        {{ old('remember') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="remember">
                                         {{ __('Se souvenir de moi') }}
                                     </label>
@@ -54,11 +54,13 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Connexion') }}
                                 </button>
-
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                    {{ __('Inscreé ?') }}
+                                </a>
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Mot de passe oublié ?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Mot de passe oublié ?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
